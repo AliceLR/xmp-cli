390b19d943d65e9ee9e7b941eecd99f09fa63182
AliceLR <petrifiedrowan@gmail.com>
2026-01-28 23:01:14 -0700

    Add support for 24-bit and 32-bit audio output.
    Drivers with 24-bit and 32-bit audio support:
    - [x] ALSA
    - [ ] BeOS/Haiku
    - [ ] CoreAudio
    - [x] NetBSD
    - [x] OSS
    - [x] PulseAudio
    - [x] sndio
    - [ ] WinMM
    - [x] AIFF file
    - [x] Raw file
    - [x] WAV file
    
    Drivers that do not support 24-bit or 32-bit:
    - [ ] Sound Blaster
    
    Drivers to verify:
    - [ ] DART
    
    Skipped drivers (I can't test them right now--they all just unset 32-bit):
    - AHI
    - AIX
    - ALSA 0.5
    - BSD
    - HP-UX
    - QNX
    - SGI
    - Solaris

--- src/sound_dart.c
+++ src/sound_dart.c
@@ -1,5 +1,5 @@
 /* Extended Module Player
- * Copyright (C) 1996-2016 Claudio Matsuoka and Hipolito Carraro Jr
+ * Copyright (C) 1996-2026 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See the COPYING
@@ -152,6 +152,7 @@ static int init(struct options *options)
 	memset(MixBuffers[1].pBuffer, /*32767 */ 0, bsize);
 	MixSetupParms.pmixWrite(MixSetupParms.ulMixHandle, MixBuffers, 2);
 
+	options->format &= ~XMP_FORMAT_32BIT;
 	return 0;
 }
 
